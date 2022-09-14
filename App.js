c0c80b6253542ecd37a3485cc29d2be4b79ece0b
NVPL1234 <nlam39784@gmail.com>
2022-09-14 22:39:52 +0700

    xong

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState("")
+  const [nameTemp, setNameTemp] = useState("")
+
+  function onPressHandle() {
+    setName(nameTemp)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.textInput} placeholder="Enter name" onChangeText={setNameTemp}></TextInput>
+      <Button title="Show test" onPress={onPressHandle}></Button>
+      <Text style={styles.text}>{name}</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +26,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  textInput: {
+    marginBottom: 10,
+    height: 50,
+    width: 400,
+    borderColor: 'blue',
+    borderWidth: 1,
+    fontSize: 25
+  },
+  text: {
+    fontSize: 25
+  }
+})
